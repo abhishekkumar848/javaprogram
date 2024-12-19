ea6449ea90408e957af2588730420cae5e8b9c44
abhishekkumar848 <153023426+abhishekkumar848@users.noreply.github.com>
2024-12-20 00:02:46 +0530

    Initial commit

---
+++ Array/arrayquestion/greaternumfind.java
@@ -0,0 +1,35 @@
+package Array.arrayquestion;
+
+import java.util.Scanner;
+
+public class greaternumfind {
+    public static int greaters(int number[],int key) {
+        int start = 0;
+        int end = number.length -1;
+        while (start<= end) {
+            int mid = (start+end)/2;
+            if (number[mid]==key) {
+                return mid;
+            }
+            if (number[mid]<key) {
+                start = mid+1;
+
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return -1;
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number[] = {1,2,3,4,5,6,20,21,35,36,37,66,64};
+        System.out.print("Enter num  = ");
+        int key = sc.nextInt();
+        int result = greaters(number, key);
+        System.out.println("this your index = " + result);
+
+
+    }
+}
