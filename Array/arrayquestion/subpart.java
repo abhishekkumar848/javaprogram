2085e70ceb83b705fc65625cc87abfb771c1e935
abhishekkumar848 <153023426+abhishekkumar848@users.noreply.github.com>
2024-12-24 01:05:56 +0530

    Updated the data upload functionality

--- Array/arrayquestion/subpart.java
+++ Array/arrayquestion/subpart.java
@@ -1,21 +1,28 @@
 package Array.arrayquestion;
 
 public class subpart {
-    public static void sub(int[] number) {
+    public static void subsum(int[] number) { // subsum is a method that takes an array of integers as an argument
+        int current = 0;
+        int max = Integer.MIN_VALUE;// ye program me maximum value ko find karne ke liye use kiya gaya hai
         for (int i = 0; i < number.length; i++) {
             int start = i;
             for (int j = i; j < number.length; j++) {
                 int end = j;
                 for (int k = start; k <= end; k++) {
                     System.out.print(number[k] + " ");
+                    current += number[k];
+
+                }
+                if (current > max) {
+                    max = current;
                 }
                 System.out.print(",");
             }
-            System.out.println();
+            System.out.println(" max sum is " + max);
         }
     }
     public static void main(String[] args) {
-        int[] number = {1,2,3,4,5,6,7,8,9};
-        sub(number);
+        int[] number = {1,2,3,4};
+        subsum(number);
     }
 }
